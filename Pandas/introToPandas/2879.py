5232b1a28bcc9a0f6282beca61ed4708722cf255
Rahimovic <rahimasselah@gmail.com>
2025-03-02 16:31:00 +0100

    Learn Pandas in LeetCode - intro to Pandas path - 30Days of Pandas pat

---
+++ Pandas/introToPandas/2879.py
@@ -0,0 +1,9 @@
+'''
+Display first three rows
+'''
+import pandas as pd
+
+def selectFirstRows(employees: pd.DataFrame) -> pd.DataFrame:
+    #return employees.iloc[0:3]
+    return employees.head(3)
+    
