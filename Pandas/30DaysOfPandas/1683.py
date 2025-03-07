e745c58e1cfc1c00cfa51527aa15654b8799904e
ARahimovic <96298305+ARahimovic@users.noreply.github.com>
2025-03-07 12:15:05 +0200

    solve panda exercices 30 Days of Pandas

---
+++ Pandas/30DaysOfPandas/1683.py
@@ -0,0 +1,48 @@
+'''
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| tweet_id       | int     |
+| content        | varchar |
++----------------+---------+
+tweet_id is the primary key (column with unique values) for this table.
+content consists of characters on an American Keyboard, and no other special characters.
+This table contains all the tweets in a social media app.
+ 
+
+Write a solution to find the IDs of the invalid tweets. The tweet is invalid if the number of characters used in the content of the tweet is strictly greater than 15.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Tweets table:
++----------+-----------------------------------+
+| tweet_id | content                           |
++----------+-----------------------------------+
+| 1        | Let us Code                       |
+| 2        | More than fifteen chars are here! |
++----------+-----------------------------------+
+Output: 
++----------+
+| tweet_id |
++----------+
+| 2        |
++----------+
+Explanation: 
+Tweet 1 has length = 11. It is a valid tweet.
+Tweet 2 has length = 33. It is an invalid tweet.
+'''
+
+import pandas as pd
+
+def invalid_tweets(tweets: pd.DataFrame) -> pd.DataFrame:
+    #using loc
+    return tweets.loc[(tweets.content.str.len() > 15 ) , ['tweet_id']]
+    #using row filtering (if I used as single brackets in tweetId it will return a series/column but double brackets it returns a data frame)
+    #return tweets[tweets['content'].str.len() > 15][['tweet_id']]
